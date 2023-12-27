db9b8d938c4c17203081bd8c8cc750f30bb84d07
Mark Parrish <mark@pipcommunications.com>
2023-12-26 17:11:06 -0800

    added the model trait formatter back

---
+++ src/ModelTraits/FormatterTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Findable\ModelTraits;
+
+trait ModelFormatterTrait
+{
+    public function formatMultiLevelBucketCounts($buckets)
+    {
+        // Transform the array and calculate totals
+        $bucket = [];
+        foreach ($buckets['buckets'] as $item) {
+            $currentLevel = &$bucket;
+            foreach ($item['key'] as $key => $value) {
+                if (!isset($currentLevel[$value])) {
+                    $currentLevel[$value] = [];
+                }
+                $currentLevel = &$currentLevel[$value];
+            }
+
+            $currentLevel = $item['doc_count'];
+        }
+
+        // Output the transformed array with totals
+        return $bucket;
+    }
+
+    public function formatSingleLevelBucketCounts($buckets)
+    {
+        // Transform the array and calculate totals
+        $bucket = [];
+        foreach ($buckets['buckets'] as $item) {
+            $bucket[$item['key']] = $item['doc_count'];
+        }
+
+        // Output the transformed array with totals
+        return $bucket;
+    }
+
+    public function formatSimpleCounts($aggregation)
+    {
+        return $aggregation['value'];
+    }
+}
